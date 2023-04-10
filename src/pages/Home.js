9fc5701a7b520b8294bd39c73af251ab0bd20e19
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-10 23:53:42 +0530

    develop demo code for how to do programmatically navigate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate()
+
+    // programmatically navigate; this is for only demo purpose
+    // this mechanism can be used to navigate automatically some timer expires
+    const navigateHandler = () => {
+        navigate("/products")
+    }
+
     return(
         <Fragment>
             <h1>Home page</h1>
             <p>Go to the <Link to="/products">list of products page</Link></p>
+            <button onClick={navigateHandler}>Products</button>
         </Fragment>
     )
 }
