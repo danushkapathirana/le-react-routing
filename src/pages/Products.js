063ca393c5b3dc5f77dbd3b7957ab23c4bb30b97
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-11 00:25:32 +0530

    add some dummy products and develop how to generate dynamic link

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,26 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
+const PRODUCTS = [
+    {id: 1, title: "Product 1"},
+    {id: 2, title: "Product 2"},
+    {id: 3, title: "Product 3"}
+]
+
 const Products = () => {
     return(
         <Fragment>
             <h1>Products page</h1>
             <p>Go to the <Link to="/">home page</Link></p>
+
+            <br />
+            <ul>
+                {
+                    PRODUCTS.map((prod) => (
+                        <li key={prod.id}> <Link to={`/products/${prod.id}`}> {prod.title} </Link> </li>
+                    ))
+                }
+            </ul>
         </Fragment>
     )
 }
